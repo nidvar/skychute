fd165f5ff8e04f1d6dc4d24faa3cb04a5309724f
Nidvar <djayshine87@hotmail.com>
2021-02-25 17:27:53 +1100

    refactor

--- src/assignment_01/jay_algorithm.js
+++ src/assignment_01/jay_algorithm.js
@@ -1,49 +1,7 @@
 const algo_check = (the_string)=>{
     let result;
-    let y = the_string
 
-    // Put brackets into an array
     let x = the_string.split('');
-
-    // Create bracket template:
-    const opening = ['{', '[', '('];
-    const closing = ['}', ']', ')'];
-
-    // Check if input is brackets only
-    let test = []
-    x.forEach(a=>{
-        if(!opening.includes(a) && !closing.includes(a)){
-            test.push(a)
-        }
-    })
-    if(test.length > 0){
-        console.log('fail. non brackets')
-        return
-    }
-
-    // Check if its even or odd. If Odd, then the sequence fails
-    if(x.length%2!=0){
-        console.log('fail. Odd number brackets =================')
-        return
-    }
-
-    // Check if sequence starts / ends with wrong brackets. If so, it fails
-    if(closing.includes(x[0]) || opening.includes(x[x.length-1])){
-        console.log('sequence fail ===============')
-        return
-    }
-
-    //If any of these are included, then fail
-    const bad_combination = ['{]', '{)', '[}', '[)', '(}', '(]']
-    bad_combination.forEach(a=>{
-        if(y.includes(a)){
-            console.log('bad_combination')
-            result = 'fail'
-            return
-        }
-    })
-
-    // Remainging tests to eliminate impurities:
 
     let one = []
     let two = []
@@ -86,4 +44,4 @@ const algo_check = (the_string)=>{
     }
 }
 
-algo_check('(([]}}')
+algo_check('{(){}[()])')
