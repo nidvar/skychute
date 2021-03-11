6fd1ba32f9da289eacdf2a3d3b06ea940b8b76f0
Nidvar <djayshine87@hotmail.com>
2021-03-11 17:13:26 +1100

    finished square root

---
+++ src/assignment_03/square_root.js
@@ -0,0 +1,37 @@
+const square_root_jay=(integer)=>{
+    let arr = [];
+    for(let i = 1; i<=integer; i++){
+        arr.push(i)
+    }
+    let start = 0;
+    let end = arr[arr.length-1];
+
+    let middle = Math.floor(integer/2);
+    while(start <= end){
+
+        if(middle*middle==integer){
+            console.log(middle)
+            return middle
+        }
+        if(middle*middle > integer){
+            end = middle+1
+            console.log('bounds', start, end)
+        }else if(middle*middle < integer){
+            start = middle-1
+            console.log('bounds', start, end)
+        }
+        
+        
+        if(middle==Math.floor((end+start)/2)){
+            break;
+        }
+
+        middle = Math.floor((end+start)/2)
+
+    }
+    return middle
+}
+
+console.log(square_root_jay(25))
+
+console.log('finished')
